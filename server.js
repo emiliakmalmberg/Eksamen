5d9666438e4f67f42c933c116ae8a2461c4849b0
emiliakmalmberg <emma20ab@student.cbs.dk>
2020-11-16 12:11:01 +0100

    First Commit

---
+++ server.js
@@ -0,0 +1,7 @@
+//Min server, bruger express
+const express = require ("express");
+
+const app = express ();
+const PORT = 8000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
