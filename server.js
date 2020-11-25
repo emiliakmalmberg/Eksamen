3dde70ffc0d92e575d89713af2b7303a666586f0
emiliakmalmberg <emma20ab@student.cbs.dk>
2020-11-25 12:00:22 +0100

    jk

--- server.js
+++ server.js
@@ -5,9 +5,3 @@ const app = express ();
 const PORT = 8000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-//Henter min controller:
-const loginController =  require('./Controller/loginController')
-
-//Mine endpoints 
-server.post('/login', loginController)
