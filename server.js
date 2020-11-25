e747bd874355016e0804fc2e6e1b9f977eaf4edf
emiliakmalmberg <emma20ab@student.cbs.dk>
2020-11-25 11:36:53 +0100

    IK

--- server.js
+++ server.js
@@ -8,11 +8,6 @@ app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 //Henter min controller:
 const loginController =  require('./Controller/loginController')
-const protectedController = require('./Controller/Protectedcontroller')
-
-//henter middleware
-const ensureToken = require('./Middleware/token')
 
 //Mine endpoints 
-server.get('/protected', ensureToken,  protectedController)
 server.post('/login', loginController)
